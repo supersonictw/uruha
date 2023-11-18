016bb8ea19a7807517571a873f7fd4d4e975bd99
Shiranui <supersonic@livemail.tw>
2023-11-18 22:24:44 +0800

    fix: update rocm.sh

--- build/rocm.sh
+++ build/rocm.sh
@@ -2,16 +2,21 @@
 
 set -e
 
-# install script dependencies
+# Install script dependencies
 apt-get -qq install build-essential cmake repo git git-lfs
 
-# repo sync
+# Repo sync
 repo init -u https://github.com/RadeonOpenCompute/ROCm.git -b roc-5.7.x
 repo sync
 
-# prepare rocm-build
-git clone https://github.com/xuhuisheng/rocm-build.git --branch 59b87dc62972c1ad32a8862e9ea6b5921c1f33a0
-cd rocm-build && source env.sh && sh install-dependency.sh
+# Prepare rocm-build
+git clone https://github.com/xuhuisheng/rocm-build.git
+cd rocm-build
+git checkout 59b87dc62972c1ad32a8862e9ea6b5921c1f33a0
+
+# Load build environment
+source env.sh
+sh install-dependency.sh
 
 # Array of script names matching the pattern [0-9][0-9].?????.sh
 scripts=($(ls -1v [0-9][0-9].*.sh))
