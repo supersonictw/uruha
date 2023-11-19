9c5d9918bd34633031c9344095ea164626e0abf5
Shiranui <supersonic@livemail.tw>
2023-11-19 22:52:24 +0800

    feat: make ipython as default shell

--- activate.sh
+++ activate.sh
@@ -14,7 +14,7 @@ do
     $SUDO mount --bind /$name $URUHA_WORK_DIRECTORY/rootfs/$name
 done
 
-$SUDO chroot rootfs
+$SUDO chroot rootfs /root/.uruha_python/bin/ipython
 
 for name in tmp proc sys dev/pts dev etc/resolv.conf
 do
