d1eae59c4af167df2d440d5cc8fb673413e2c28e
SuperSonic <supersonic@livemail.tw>
2023-11-19 15:13:06 +0000

    fix: typos

--- init_rootfs.sh
+++ init_rootfs.sh
@@ -11,9 +11,10 @@ export URUHA_WORK_DIRECTORY=$(pwd)
 
 $SUDO wget -O $URUHA_WORK_DIRECTORY/rootfs.tar.gz http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.1-base-amd64.tar.gz
 
-$SUDO mkdir $URUHA_WORK_DIRECTORY/rootfs && cd $URUHA_WORK_DIRECTORY/rootfs
-$SUDO tar -zxvf $URUHA_WORK_DIRECTORY/rootfs.tar.gz
-rm $URUHA_WORK_DIRECTORY/rootfs
+$SUDO mkdir $URUHA_WORK_DIRECTORY/rootfs
+
+$SUDO tar -zxvf $URUHA_WORK_DIRECTORY/rootfs.tar.gz -C $URUHA_WORK_DIRECTORY/rootfs
+$SUDO rm $URUHA_WORK_DIRECTORY/rootfs.tar.gz
 
 $SUDO cp $URUHA_WORK_DIRECTORY/.rootfs_setup.sh $URUHA_WORK_DIRECTORY/rootfs/setup.sh
 chmod +x $URUHA_WORK_DIRECTORY/rootfs/setup.sh
