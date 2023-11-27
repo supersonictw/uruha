bf28b5c6941ea155ae9524fc9a8d03bdf5dd4478
Shiranui <supersonic@livemail.tw>
2023-11-27 19:48:24 +0800

    feat: update activate.sh

--- activate.sh
+++ activate.sh
@@ -4,28 +4,46 @@ set -e
 
 export URUHA_WORK_DIRECTORY=$(pwd)
 
-if [ ! -f "/tmp/uruha.lock" ]; then
-    touch /tmp/uruha.lock
-else
-    echo "/tmp/uruha.lock already exists, exits for preventing from unexpected mounting."
-    exit 1
-fi
-
 SUDO=''
 if [ "$EUID" != 0 ]; then
     SUDO='sudo'
 fi
 
-for name in tmp proc sys dev dev/pts etc/resolv.conf
-do
-    $SUDO mount --bind /$name $URUHA_WORK_DIRECTORY/rootfs/$name
-done
+URUHA_MOUNT() {
+    if [ ! -f "/tmp/uruha.lock" ]; then
+        touch /tmp/uruha.lock
+    else
+        echo "/tmp/uruha.lock already exists, exits for preventing from unexpected mounting."
+        exit 1
+    fi
 
-$SUDO chroot rootfs /bin/sh -c "cd /root && bash"
+    for name in tmp proc sys dev dev/pts etc/resolv.conf
+    do
+        $SUDO mount --bind /$name $URUHA_WORK_DIRECTORY/rootfs/$name
+    done
+}
 
-for name in tmp proc sys dev/pts dev etc/resolv.conf
-do
-    $SUDO umount $URUHA_WORK_DIRECTORY/rootfs/$name
-done
+URUHA_UMOUNT() {
+    for name in tmp proc sys dev/pts dev etc/resolv.conf
+    do
+        $SUDO umount $URUHA_WORK_DIRECTORY/rootfs/$name
+    done
 
-rm /tmp/uruha.lock
+    rm /tmp/uruha.lock
+}
+
+URUHA_CHROOT() {
+    $SUDO chroot rootfs /bin/sh -c "cd /root && bash"
+}
+
+case "$1" in
+    "") 
+        URUHA_MOUNT
+        URUHA_CHROOT
+        URUHA_UMOUNT
+        ;;
+    umount)
+        URUHA_UMOUNT
+        ;;
+    *) echo "$1: unknown task."
+esac
