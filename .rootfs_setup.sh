be5ccfcd5d803b429a19f75b8a734249d3ca1adc
Shiranui <supersonic@livemail.tw>
2024-03-18 17:50:05 +0800

    fix: add quotes for preventing from exception

--- .rootfs_setup.sh
+++ .rootfs_setup.sh
@@ -21,8 +21,8 @@ tee -a /root/.bashrc <<'EOF'
 export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u [uruha_chroot]\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 
 export HSA_OVERRIDE_GFX_VERSION="10.3.0"
-export ROCM_PATH=/opt/rocm
-export PATH=$ROCM_PATH/bin:$PATH
+export ROCM_PATH="/opt/rocm"
+export PATH="$ROCM_PATH/bin:$PATH"
 
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
